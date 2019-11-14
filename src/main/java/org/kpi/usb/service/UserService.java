641f46d0db39cf36edf4edfc541c13005dd96962
Yurii Matora <yura.matora11@gmail.com>
2019-11-14 23:01:13 +0200

    Create user service and add method to get user variant by github id

---
+++ src/main/java/org/kpi/usb/service/UserService.java
@@ -0,0 +1,43 @@
+package org.kpi.usb.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+
+@Service
+@Slf4j
+public class UserService {
+    private String persistenceServerAddr;
+
+    public UserService(@Value("labchecker.persistence-server.address") String persistenceServerAddr) {
+        this.persistenceServerAddr = persistenceServerAddr;
+    }
+
+    public Optional<Integer> getUserVariantByGithubID(long githubID) {
+        final String userVariantEndpoint = "/api/v1/students/{githubID}/variant";
+
+        Map<String, String> params = new HashMap<>();
+        params.put("githubID", String.valueOf(githubID));
+
+        RestTemplate restTemplate = new RestTemplate();
+        String variant = restTemplate.getForObject(persistenceServerAddr + userVariantEndpoint, String.class, params);
+
+        if (Objects.isNull(variant)) {
+            log.warn("There is no variant for such user with githubID: {}", githubID);
+            return Optional.empty();
+        } else {
+            try {
+                return Optional.of(Integer.parseInt(variant));
+            } catch (NumberFormatException e) {
+                log.error("Can not parse user variant", e);
+                return Optional.empty();
+            }
+        }
+    }
+}
