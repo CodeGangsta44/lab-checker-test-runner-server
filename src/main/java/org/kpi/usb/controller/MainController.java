af5c6f921b1a75af9238f533f175eed12025174c
Yurii Matora <yura.matora11@gmail.com>
2019-11-28 22:17:00 +0200

    Add logging for main controller

--- src/main/java/org/kpi/usb/controller/MainController.java
+++ src/main/java/org/kpi/usb/controller/MainController.java
@@ -1,5 +1,6 @@
 package org.kpi.usb.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.kpi.usb.entity.PullRequest;
 import org.kpi.usb.entity.Repository;
 import org.kpi.usb.entity.Result;
@@ -11,6 +12,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/")
+@Slf4j
 public class MainController {
 
     private TestingService testingService;
@@ -38,21 +40,33 @@ public class MainController {
 
     @PostMapping
     public void receiveWebHook(@RequestBody String content) {
+        log.info("Start parsing entities from json webhook");
         Repository repository = githubWebHookService.getRepositoryFromJSON(content);
         Student student = githubWebHookService.getStudentFromJSON(content);
         PullRequest pullRequest = githubWebHookService.getPullRequestFromJSON(content);
+        log.info("Entities parsed successfully");
+        log.info("Current student login: {}", student.getLogin());
 
+        log.info("Start receiving variant for student");
         Optional<Integer> studentVariantOptional = userService.getUserVariantByGithubIDAndLabRepo(student.getId(), repository.getName());
+        log.info("Start receiving maximal mark for lab for student");
         Optional<Integer> maxMarkForLab = labService.getMaxMarkForLab(repository.getName());
 
         int studentVariant = studentVariantOptional.orElseThrow();
+        log.info("Student variant: {}", studentVariant);
         int maxMark = maxMarkForLab.orElseThrow();
+        log.info("Max mark for lab: {}", maxMark);
 
+
+        log.info("Start testing lab {} for student {}", repository.getName(), student.getLogin());
         final int mark = testingService.runTest(repository.getName(),
                 student.getLogin(),
                 studentVariant,
                 maxMark);
+        log.info("Lab was tested, student mark is - {}", mark);
 
+
+        log.info("Building lab result");
         Result result = Result.builder()
                 .studentGithubLogin(student.getLogin())
                 .studentGithubID(student.getId())
@@ -62,7 +76,9 @@ public class MainController {
                 .language(repository.getLanguage())
                 .updatedDate(pullRequest.getUpdatedDate())
                 .build();
+        log.info("Student result: {}", result);
 
+        log.info("Sending result to persistence server");
         resultService.sendResultToPersistenceServer(result);
     }
 
